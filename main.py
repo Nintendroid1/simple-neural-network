4f8ec1e2b98f51ab4b3d76440e7c9ec35e11f50f
Nintendroid1 <nlpsalazar@gmail.com>
2019-12-31 19:06:40 -0500

    Adapting neural network code to arrest data

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from numpy import exp, array, random, dot
+import sys, csv
 
-
+#Source code: https://github.com/miloharper/simple-neural-network
 class NeuralNetwork():
     def __init__(self):
         # Seed the random number generator, so it generates the same numbers
@@ -27,7 +28,7 @@ class NeuralNetwork():
     # We train the neural network through a process of trial and error.
     # Adjusting the synaptic weights each time.
     def train(self, training_set_inputs, training_set_outputs, number_of_training_iterations):
-        for iteration in xrange(number_of_training_iterations):
+        for iteration in range(number_of_training_iterations):
             # Pass the training set through our neural network (a single neuron).
             output = self.think(training_set_inputs)
 
@@ -48,27 +49,46 @@ class NeuralNetwork():
         # Pass inputs through our neural network (our single neuron).
         return self.__sigmoid(dot(inputs, self.synaptic_weights))
 
+#Parses the csv data
+def parseData(file_name):
+    with open(file_name) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                #Prints all column names
+                #print(f'Column names are {", ".join(row)}')
 
+                print(str(row[6]) + "\t" + str(row[9]) + "\t" + str(row[10]) + "\t" + str(row[11]))
+                line_count += 1
+            else:
+                print(str(row[6]) + "\t" + str(row[9]) + "\t\t" + str(row[10]) + "\t\t" + str(row[11]))
+                line_count += 1
 if __name__ == "__main__":
+    if(len(sys.argv) == 2):
+        #Intialise a single neuron neural network.
+        neural_network = NeuralNetwork()
 
-    #Intialise a single neuron neural network.
-    neural_network = NeuralNetwork()
+        print("Random starting synaptic weights: ")
+        print(neural_network.synaptic_weights)
 
-    print "Random starting synaptic weights: "
-    print neural_network.synaptic_weights
+        # The training set. We have 4 examples, each consisting of 3 input values
+        # and 1 output value.
+        training_set_inputs = array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]])
+        training_set_outputs = array([[0, 1, 1, 0]]).T
 
-    # The training set. We have 4 examples, each consisting of 3 input values
-    # and 1 output value.
-    training_set_inputs = array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]])
-    training_set_outputs = array([[0, 1, 1, 0]]).T
+        # Train the neural network using a training set.
+        # Do it 10,000 times and make small adjustments each time.
+        neural_network.train(training_set_inputs, training_set_outputs, 10000)
 
-    # Train the neural network using a training set.
-    # Do it 10,000 times and make small adjustments each time.
-    neural_network.train(training_set_inputs, training_set_outputs, 10000)
+        print("New synaptic weights after training: ")
+        print(neural_network.synaptic_weights)
 
-    print "New synaptic weights after training: "
-    print neural_network.synaptic_weights
+        # Test the neural network with a new situation.
+        print("Considering new situation [1, 0, 0] -> ?: ")
+        print(neural_network.think(array([1, 0, 0])))
 
-    # Test the neural network with a new situation.
-    print "Considering new situation [1, 0, 0] -> ?: "
-    print neural_network.think(array([1, 0, 0]))
+        # Read csv file
+        parseData(sys.argv[1])
+    else:
+        print("Please input file name for training data for neural network") 
